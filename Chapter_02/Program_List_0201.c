eced9fbf28ca98a9f034d3aae2e032cc0ef86a53
Hunry <Chen_Nanzhu666@163.com>
2023-02-08 07:04:38 +0800

    chapter two

---
+++ Chapter_02/Program_List_0201.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void) {
+    int num;
+    num = 1;
+    printf("I am a simple ");
+    printf("computer.\n");
+    printf("My favorate number is %d becoause it is first.\n",num);
+    return 0;
+}
